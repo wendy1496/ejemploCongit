1f5721584337407ec3ed65c98a5dce7a5ed6c04f
wendy1496 <wendyrendonf@gmail.com>
2023-02-17 13:37:24 -0500

    Primer repositorio

---
+++ ejercicio.js
@@ -0,0 +1,25 @@
+let array = [{
+        id: 0,
+        nombre: "obj 1",
+        fechaI: new Date('2023-01-12').getTime(),
+        fechaF: new Date('2023-02-12').getTime()
+    },
+    {
+        id: 1,
+        nombre: "obj 2",
+        fechaI: new Date('2023-02-13').getTime(),
+        fechaF: new Date('2023-03-12').getTime()
+    }
+
+]  
+
+function diasDiferencia(diasDeDiferencia){
+    for(let i = 0; i < array.length; i++){
+        let diferencia = array[i].fechaF - array[i].fechaI
+        let diasDeDiferencia = diferencia / (1000*60*60*24)
+        console.log(diasDeDiferencia);
+    }
+    return diasDeDiferencia
+}
+
+correrArreglo();
